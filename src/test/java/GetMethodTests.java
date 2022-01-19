792407e2aed448f7510a5f350da763eb0d4a02dd
Rajender Singh Harit <rajendersinghharit@Rajenders-MacBook-Pro.local>
2022-01-19 18:57:31 +0530

    Adding assertions on attribute

--- src/test/java/GetMethodTests.java
+++ src/test/java/GetMethodTests.java
@@ -1,4 +1,5 @@
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,19 +15,38 @@ public class GetMethodTests {
         Assert.assertEquals(statusCode, 200, "Unexpected status code");
     }
 
+
+
+    @Test(description = "Gets the list of all users and validates limit of users array is 20")
+    public void getAllUsersLimitIs20(){
+                given()
+                .when()
+                .get("https://gorest.co.in/public/v1/users")
+                .then()
+                .statusCode(200)
+                .body("data", Matchers.hasSize(20));
+
+    }
+
     @Test(description = "Create a user")
     public void shouldCreateUser(){
+        int randomNum = (int)(Math.random()*10000);
+        String name = "Tenali Ramakrishna";
         given()
                 .accept(ContentType.JSON)
                 .contentType(ContentType.JSON)
                 .header("Authorization",
                         "Bearer 0a5cb4fbc5ef2e2c87e1c600fd2961a2041c9cde441d69c1dc4b8137a7cc663c")
-                .body("{\"name\":\"Tenali Ramakrishna\"," +
+                .body("{\"name\":\""+ name+"\"," +
                         " \"gender\":\"male\"," +
-                        " \"email\":\"tenali.ramakrishna@15ce.com\"," +
+                        " \"email\":\"tenali.ramakrishna" + randomNum + "@15ce.com\" ," +
                         " \"status\":\"active\"}")
-                .when()
-                .post("https://gorest.co.in/public/v1/users")
-                .then().log().body().statusCode(201);
+            .when()
+            .post("https://gorest.co.in/public/v1/users")
+            .then()
+                .log()
+                .body()
+                .statusCode(201)
+                .body("data.name", Matchers.equalTo(name));
     }
 }
