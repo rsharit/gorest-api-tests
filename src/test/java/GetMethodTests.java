3fc2114d1b00adbaccd0bd049f160cd6d1801ded
Rajender Singh Harit <rajendersinghharit@Rajenders-MacBook-Pro.local>
2022-01-19 17:22:06 +0530

    creating a user, but would get 422

--- src/test/java/GetMethodTests.java
+++ src/test/java/GetMethodTests.java
@@ -1,15 +1,32 @@
+import io.restassured.http.ContentType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class GetMethodTests {
-    @Test
-    public void getUsers(){
+    @Test(description = "Gets the list of all users and validates the status code only")
+    public void shouldGetAllUsers(){
         int statusCode = given()
                 .when()
                 .get("https://gorest.co.in/public/v1/users")
                 .getStatusCode();
         Assert.assertEquals(statusCode, 200, "Unexpected status code");
+    }
+
+    @Test(description = "Create a user")
+    public void shouldCreateUser(){
+        given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .header("Authorization",
+                        "Bearer 0a5cb4fbc5ef2e2c87e1c600fd2961a2041c9cde441d69c1dc4b8137a7cc663c")
+                .body("{\"name\":\"Tenali Ramakrishna\"," +
+                        " \"gender\":\"male\"," +
+                        " \"email\":\"tenali.ramakrishna@15ce.com\"," +
+                        " \"status\":\"active\"}")
+                .when()
+                .post("https://gorest.co.in/public/v1/users")
+                .then().log().body().statusCode(201);
     }
 }
